16739c0ca4516dd817a0a60ecf4543b35cc03424
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-02-20 22:27:20 -0600

    Update -> comment endpoints unnecessaries

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const PORT = 3000;  // nuestra constante PORT o puerto 
-const {users} = require("./data")
+//const {users} = require("./data")
+
+const authRouter = require("./routes/authRoutes");
+const greetRouter = require("./routes/greetRoutes");
 
 // Crear la instacia del servidor
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//
+app.use("/auth", authRouter);
+app.use("/greet", greetRouter);
+
 app.get('/',(req,res)=>{
     res.json({msj: "Hola desde express usando json"})
 });
 
-app.get('/usr',(req,res)=>{
+/*app.get('/usr',(req,res)=>{
 
     res.json(users)
-})
+})*/
 
-app.post("/login",(req, res)=>{
+/*app.post("/login",(req, res)=>{
     const {username, password} = req.body;
-    const user = user.find((usr)=>{
+    const user = users.find((usr)=>{
         usr.username === username &&
         usr.password === password
     });
@@ -30,30 +37,21 @@ app.post("/login",(req, res)=>{
         res.status(401).json({error:"Usuario no válido"})
     }
 
-})
+})*/
 
-const validarToken= (req, res, next)=>{
-    const token = req.headers.authorization;
-    if(token){
-        if(token.startsWith("token-falso-"))
-            next();
-        res.status(403).json({message:"Acceso denegado por token falso"})
-    }else{
-        res.status(403).json({
-            message: "Acceso denegado. No hay token"
-        })
-    }
-}
 
 //Saludo protegido
-app.get("/saludo-protegido", validarToken, (req,res)=>{
+/*app.get("/saludo-protegido", validarToken, (req,res)=>{
     res.json({
         message:"Saludo Protegido"
     });
-})
+})*/
 
 
 //definir nuestra escucha
 app.listen(PORT,()=>{
     console.log("Server running in http://localhost:"+PORT)
 });
+
+//ARREGLAR EL TOKEN PARA QUE LANCE EL TOKEN VÁLIDO
+//Token debe de regresar con token-falso-id
