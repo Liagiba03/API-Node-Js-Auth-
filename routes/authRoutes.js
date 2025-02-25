150f3dac591c751031e22e827917ab6564c29584
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-02-24 23:14:27 -0600

    Update add endpoint for user registration

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
-const {getUsers, loginController} = require("../Controller/authController");
+const {getUsers, loginController, createUserController} = require("../Controller/authController");
 const router = require("express").Router();
 
 router.get("/users", getUsers);
 
 router.post("/login",loginController);
+router.post("/create-user",createUserController)
 
 module.exports = router;
