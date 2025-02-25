411cc72f1ffa56f7cf4e97a07d5f40ba79b0790c
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-02-24 23:15:21 -0600

    Update functions

--- Service/authService.js
+++ Service/authService.js
@@ -1,29 +1,56 @@
 const {users} = require("../data/users.js");
+const { User } = require("../models/userModel.js");
 
 //Funcion para la primer ruta
 
 //get --> /get-users
-const getAllUsers =() =>{
-    return users;
+const getAllUsers = async () =>{
+    try {
+        const users =await User.find();
+        return users;
+    } catch (error) {
+        console.log(error);
+        return null;
+        
+    }
+}
+
+//CREATE USERS
+const createUser = async (username, password)=>{
+    try {
+        const newUser = new User({username, password})
+        const savedUser = await newUser.save();
+        return savedUser;
+    } catch (error) {
+        return null;
+    }
 }
 
 //get -->login
-const login =(user, pass) =>{
+const login = async (user, pass) =>{
     //COLOCAR BIEN EL TOKEN token-falso-1
-    var token = "token-falso-";
-    //const validate= getUserByNameAndPass(user, pass)
+    
+    const validate= await getUserByNameAndPass(user, pass);
     //console.log(validate);
     // Validar usuario
-    //if (validate){
-    if (getUserByNameAndPass(user, pass)){
-       token += getUserByNameAndPass(user, pass).id;
+    if (validate){
+    //if (getUserByNameAndPass(user, pass)){
+        var token = `token-falso-${validate._id}`;
+       //token += getUserByNameAndPass(user, pass).id;
         return {token};
+    }else{
+        return {message: "usuario o contraseña incorrectos"};
     }
-    return {message: "usuario o contraseña incorrectos"};
 }
 
-const getUserByNameAndPass = (user, pass) =>{
-    return users.find(usr => usr.username === user && usr.password === pass);
+const getUserByNameAndPass = async (user, pass) =>{
+    //return users.find(usr => usr.username === user && usr.password === pass);
+    try {
+        const user = User.findOne({user, pass});
+        return user;
+    } catch (error) {
+        return null;
+    }
 }
 
-module.exports= {getAllUsers, login}
+module.exports= {getAllUsers, login, createUser}
