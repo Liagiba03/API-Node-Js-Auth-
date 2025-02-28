3de5a50aeb08b0a9f0e4c14f3ca4238ab416ccbd
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-02-28 14:38:56 -0600

    Update authController.js->Login

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,36 +1,18 @@
-const {getAllUsers,login, createUser} = require("../Service/authService.js");
+const {login} = require("../Service/authService.js");
 
-const getUsers = async (req, res) =>{
-    //const users = getAllUsers();
-    const users = await getAllUsers();
-    res.json(users)
-}
 
 const loginController = async (req, res) =>{
     const {username, password} = req.body;
     const respuesta = await login(username, password);
-    const {token} = respuesta;
-    const {message} = respuesta;
-
-    if(token){
-        return res.json({token: token});
+    if (respuesta.status ===200){
+        res.json({token: respuesta.token});
+    }else{
+        res.status(respuesta.status).json({messsage:respuesta.message})
     }
-    res.status(401).json({message: message}); 
 
 }   
 
 
-//ENdpoint CreateUser
 
-const createUserController = async (req, res) =>{
-    const {username, password} = req.body;
-    const newUser = await createUser(username, password);
-    if(newUser){
-        return res.status(201).json(newUser);
-    }else{
-        return res.status(500).json({message:`DB not connected`})
-    }
-}
-
-module.exports = {getUsers, loginController, createUserController
+module.exports = {loginController
 }
