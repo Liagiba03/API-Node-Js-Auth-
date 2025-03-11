98b7f400fc261a1ed60bc5b5d439f2467c1d8915
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-03-10 23:09:35 -0600

    Update add singup route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
-const {loginController} = require("../Controller/authController");
+const {loginController, createUsrController} = require("../Controller/authController");
 const router = require("express").Router();
 
 //router.get("/users", getUsers);
 
 router.post("/login",loginController);
+router.post("/signup",createUsrController);
 //router.post("/create-user",createUserController)
 
 module.exports = router;
